46c1d99c9ded377f04ec1c42c48fbb7741cb28e9
Gopisam1126 <gopisambhu1126@gmail.com>
2023-09-26 21:06:00 +0530

    Second commit

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ loginLink.addEventListener("mouseout", () => {
     arrow.style.transform = "scaleX(1)";
     arrow.style.marginRight = "0rem";
 });
+
+// Side Navbar
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+// sidebar Toggle
+
+var sideBar = document.querySelector("#mySidebar");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    sideBar.style.width = "15rem";
+  } else {
+    sideBar.style.width = "0rem";
+  }
+}
