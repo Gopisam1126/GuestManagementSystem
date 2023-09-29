ba93bf5492f970f7fd3b94cbe3855dca55f53c6e
Gopisam1126 <gopisambhu1126@gmail.com>
2023-09-29 16:36:56 +0530

    add modal functionality

--- script.js
+++ script.js
@@ -10,29 +10,36 @@ loginLink.addEventListener("mouseout", () => {
     arrow.style.marginRight = "0rem";
 });
 
-// Side Navbar
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
-
-// sidebar Toggle
-
-var sideBar = document.querySelector("#mySidebar");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    sideBar.style.width = "15rem";
-  } else {
-    sideBar.style.width = "0rem";
-  }
-}
-
 var favIcon = document.querySelectorAll(".stauts");
+
+
+// Modal 
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden")
+}
+
+// const openModal = function () {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
+
+// openModalBtn.addEventListener("click", openModal);
+
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// }
+
+// closeModalBtn.addEventListener("click", closeModal);
