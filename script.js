e3713b44e9fd77f4d422cbf7f1bb7fb8b8a75af0
Gopisam1126 <gopisambhu1126@gmail.com>
2023-09-29 23:33:56 +0530

    minor changes in code

--- script.js
+++ script.js
@@ -15,31 +15,41 @@ var favIcon = document.querySelectorAll(".stauts");
 
 // Modal 
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+document.addEventListener("DOMContentLoaded", function() {
+  // Select the open and close buttons, modal, and overlay elements
+  var openModalBtn = document.querySelector(".btn-open");
+  var closeModalBtn = document.querySelector(".btn-close");
+  var modal = document.querySelector(".modal");
+  var overlay = document.querySelector(".overlay");
 
-function openModal() {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  // Check if the open button exists and add an event listener to it
+  if (openModalBtn) {
+    openModalBtn.addEventListener("click", function() {
+      // Remove the "hidden" class from the modal and overlay to display them
+      modal.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+    });
+  }
+  // Check if the close button exists and add an event listener to it
+  if (closeModalBtn) {
+    closeModalBtn.addEventListener("click", function() {
+      // Add the "hidden" class to the modal and overlay to hide them
+      modal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    });
+  }
+});
+
+
+// Pass Verification
+
+var user = document.querySelector("#userName").value;
+var pass = document.querySelector("#pass").value;
+
+function verifyLogin() {
+  if (user === "Gopikrishnan1126" && pass === "12345678") { 
+    alert("verified..!");
+  } else {
+    alert("failed..!!!");
+  }
 }
-
-function closeModal() {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden")
-}
-
-// const openModal = function () {
-//   modal.classList.remove("hidden");
-//   overlay.classList.remove("hidden");
-// };
-
-// openModalBtn.addEventListener("click", openModal);
-
-// const closeModal = function () {
-//   modal.classList.add("hidden");
-//   overlay.classList.add("hidden");
-// }
-
-// closeModalBtn.addEventListener("click", closeModal);
