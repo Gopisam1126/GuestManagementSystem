6e330f8e5244b63e5d3a6e8f47dd71bc3c998801
scriptKid <gopikrishnan0045@gmail.com>
2024-09-11 01:20:09 +0530

    add logo

--- FRONTEND/src/assets/component/header.jsx
+++ FRONTEND/src/assets/component/header.jsx
@@ -1,7 +1,15 @@
+import "../componentStyles/common.css";
+import "../pageStyles/home.css";
+import Navbar from "./navbar";
 function Header() {
     return <>
         <section className="header-section">
-            <h1>Header</h1>
+            <div className="bg-image-container">
+                <div className="nav-logo">
+                    <img src="\logo-no-background-removebg-preview.png" alt="logo" className="logo" />
+                    <Navbar/>
+                </div>
+            </div>
         </section>
     </>
 }
