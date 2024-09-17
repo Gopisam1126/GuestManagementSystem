fd4562d442f8b635e842fcd8d5e1e9248c71d50d
scriptKid <gopikrishnan0045@gmail.com>
2024-09-18 00:00:57 +0530

    create loader

---
+++ FRONTEND/src/assets/component/loader.jsx
@@ -0,0 +1,11 @@
+function Loader() {
+    return <>
+        <section className="loader-sec">
+            <div className="loader">
+              <div className="inner_loader"></div>
+            </div>
+        </section>
+    </>
+}
+
+export default Loader;
