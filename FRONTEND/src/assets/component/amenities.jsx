ab3119992feab58d0f25682637c244b9a0640a27
scriptKid <gopikrishnan0045@gmail.com>
2024-09-19 22:42:47 +0530

    style amenities

--- FRONTEND/src/assets/component/amenities.jsx
+++ FRONTEND/src/assets/component/amenities.jsx
@@ -6,42 +6,44 @@ function Amenities() {
                 Amenities
             </h4>
             <table className="amenities-table">
-                <tr>
-                    <td className="detail-label">Size</td>
-                    <td className="detail-value">175 square feet</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Entertainment</td>
-                    <td className="detail-value">46 Inch LED TV</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Connectivity</td>
-                    <td className="detail-value">Free Wi-Fi</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Butler Service</td>
-                    <td className="detail-value">Not Available</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Guests</td>
-                    <td className="detail-value">Up to 7 guests</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Location</td>
-                    <td className="detail-value">Front side of hotel</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Occupancy</td>
-                    <td className="detail-value">2 adults and 2 children</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Refreshment</td>
-                    <td className="detail-value">Minibar</td>
-                </tr>
-                <tr>
-                    <td className="detail-label">Extras</td>
-                    <td className="detail-value">Jacuzzi</td>
-                </tr>
+                <tbody className="t-body">
+                    <tr className="table-row tr-1">
+                        <td className="detail-label dl-1">Size</td>
+                        <td className="detail-value">175 square feet</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Entertainment</td>
+                        <td className="detail-value">46 Inch LED TV</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Connectivity</td>
+                        <td className="detail-value">Free Wi-Fi</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Butler Service</td>
+                        <td className="detail-value">Not Available</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Guests</td>
+                        <td className="detail-value">Up to 7 guests</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Location</td>
+                        <td className="detail-value">Front side of hotel</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Occupancy</td>
+                        <td className="detail-value">2 adults and 2 children</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Refreshment</td>
+                        <td className="detail-value">Minibar</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td className="detail-label">Extras</td>
+                        <td className="detail-value">Jacuzzi</td>
+                    </tr>
+                </tbody>
             </table>
         </section>
     </>
