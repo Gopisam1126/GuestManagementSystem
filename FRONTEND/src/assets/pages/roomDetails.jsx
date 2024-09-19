e88b24418fa96aac29a83eddf9c924cd5f8f3dee
scriptKid <gopikrishnan0045@gmail.com>
2024-09-19 22:23:12 +0530

    mod RD component

--- FRONTEND/src/assets/pages/roomDetails.jsx
+++ FRONTEND/src/assets/pages/roomDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import Rooms from "../constants/constants";
+import Header2 from "../component/header2";
 import "../pageStyles/roomDetails.css";
+import Amenities from "../component/amenities";
 function RoomDetails() {
 
     const { id } = useParams();
@@ -12,11 +14,21 @@ function RoomDetails() {
 
     return <>
         <section className="room-detail-section">
-            <img src={room.imgUrl} alt={room.name} className="room-detail-thumbnail" />
-            <h2 className="room-detail-name">{room.name}</h2>
-            <p className="room-detail-type">{room.type}</p>
-            <p className="room-detail-price">{room.price} <span> / Night</span></p>
-            <p className="room-detail-description">Detailed description of the room goes here...</p>
+            <div className="room-details-header">
+                <Header2/>
+            </div>
+            <div className="room-det-body-container">
+                <div className="left-details">
+                    <img src={room.imgUrl} alt={room.name} className="room-detail-thumbnail" />
+                    <h4 className="room-detail-name">{room.name}</h4>
+                    <p className="room-detail-type">{room.type}</p>
+                    <p className="room-detail-price">{room.price}<span> / Night</span></p>
+                    {/* <p className="room-detail-description">Detailed description of the room goes here...</p> */}
+                </div>
+                <div className="right-aminities">
+                    <Amenities/>
+                </div>
+            </div>
         </section>
     </>
 }
